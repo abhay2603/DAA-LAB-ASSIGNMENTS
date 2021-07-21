185e163d84b72ec4308af950bc4f9ffd84ec5afe
Abhay Pratap Singh <apsr0316@gmail.com>
2021-07-22 00:24:54 +0530

    Week 1 Question 2 (question 1 was completed in week 0 itself)

---
+++ WEEK 1/binarysearch.java
@@ -0,0 +1,48 @@
+import java.util.*;
+class binarysearch
+{
+    
+    public static int bsearch(int []arr,int l,int h, int x)
+    {
+        
+        if(l>h)                        //base case
+        return -1;
+        int mid=(l+h)/2;
+        
+        if(arr[mid]==x)
+        return mid;
+        else if(arr[mid]>x)
+        return bsearch(arr,l,mid-1,x);
+        else
+        return bsearch(arr,mid+1,h,x);
+    }
+
+    //driver code
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size of array");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter array elements");
+        for(int i=0; i<n; i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter the element to be searched");
+        int x=sc.nextInt();
+ 
+        // Function call
+    
+        int found_index = bsearch(arr,0,n-1,x);
+        if (found_index == -1)
+        {
+            System.out.println("Element is not present in array");
+        }
+        else
+        {
+            System.out.println("Element is present at index" + found_index);
+        }
+        sc.close();
+    }
+}
